8892279800dfa3fd5fb7d95adfe24bbab3ea9541
mohamedessamhashim <mohamedessamhashim@gmail.com>
2026-01-30 22:58:20 +0200

    Define Visiualize

--- app.py
+++ app.py
@@ -10,7 +10,47 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/visualize", methods=["POST"])
+def visualize():
+    a1 = float(request.form['a1'])
+    b1 = float(request.form['b1'])
+    c1 = float(request.form['c1'])
+    d1 = float(request.form['d1'])
 
+    a2 = float(request.form['a2'])
+    b2 = float(request.form['b2'])
+    c2 = float(request.form['c2'])
+    d2 = float(request.form['d2'])
+
+    a3 = float(request.form['a3'])
+    b3 = float(request.form['b3'])
+    c3 = float(request.form['c3'])
+    d3 = float(request.form['d3'])
+
+    A = np.array([
+        [a1, b1, c1],
+        [a2, b2, c2],
+        [a3, b3, c3]
+        ])
+    
+    b = np.array([d1, d2, d3])
+
+    rank_A = np.linalg.matrix_rank(A)
+
+    augmented = np.column_stack([A, b])
+    rank_augmented = np.linalg.matrix_rank(augmented)
+
+    if rank_A < rank_augmented:
+        solution_type = "No Solution"
+        solution = None
+    elif rank_A == 3: 
+        solution_type = "Unique Solution"
+        solution = np.linalg.solve(A, b)
+    else:
+        solution_type = "Infinite Solutions"
+        solution = None
+    
+    return f"<h1>Solution Type: {solution_type}</h1><p>Solution: {solution}</p>"
 
 
 if __name__ == "__main__":
